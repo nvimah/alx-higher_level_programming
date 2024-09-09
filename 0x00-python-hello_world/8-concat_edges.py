5346257711392514114fd3504970183a27b99121
Sharon Wainaina <navion8759@gmail.com>
2024-09-09 13:30:25 -0700

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-
-print(f"{str[47:74]} with {str[-7:-1]}")
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
