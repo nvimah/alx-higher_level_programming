d20f33dfa8f257c01888730e5dae3cc3a91f7fe9
Sharon Wainaina <navion8759@gmail.com>
2024-09-08 21:51:48 -0700

    python files

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+
+print(f"{str[47:74]} with {str[-7:-1]}")
